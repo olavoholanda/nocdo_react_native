ec24e539d922a46ca89efd7fbf2d0713e80978ad
Olavo Holanda <olavohcn@gmail.com>
2019-03-19 16:06:11 -0300

    Fixed input keyboard on login page

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,82 +1,70 @@
-import React from 'react';
+import React from 'react'
 import {
-    StyleSheet,
-    View,
-    Image,
-    Text,
-    KeyboardAvoidingView,
-    ScrollView
-} from 'react-native';
-import { Header } from 'react-navigation';
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+} from 'react-native'
 
-
-import {Button} from 'react-native-material-ui';
-import {TextField} from 'react-native-material-textfield';
-import {mainColor, backgroundColor, accentColor} from '../constants/Colors';
-import logo from '../assets/images/logo/white.png'
+import { Button, Toolbar } from 'react-native-material-ui'
+import { TextField } from 'react-native-material-textfield'
+import { mainColor, backgroundColor, accentColor } from '../constants/Colors'
 
 export default class SignInScreen extends React.Component {
-    state = {
-        email: '',
-        password: '',
-    };
+  state = {
+    email: '',
+    password: '',
+  }
 
-    render() {
-        let {email, password} = this.state
-        return (
-            <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
-                <ScrollView contentContainerStyle={{ justifyContent: "flex-end", flex: 1 }}>
-                    <View style={styles.logoContainer}>
-                        <Image source={logo} style={styles.logoImage}/>
-                    </View>
-                    <View style={styles.loginContainer}>
-                        <View style={{flex: 3}}>
-                            <TextField tintColor={mainColor} label={'Email'} value={email}
-                                       onChangeText={email => this.setState({email})}/>
-                            <TextField secureTextEntry={true} tintColor={mainColor} label='Senha' value={password}
-                                       onChangeText={password => this.setState({password})}/>
-                        </View>
-                        <View style={{flex: 1}}>
-                            <Button raised primary text='Entrar'/>
-                        </View>
-                        <View style={{flex: 1, alignItems: 'center'}}>
-                            <Text style={styles.forgotPassword}
-                                  onPress={() => this.props.navigation.navigate('ForgotPassword')}
-                            >Esqueceu a senha?</Text>
-                        </View>
-                        <View style={{flex: 1}}>
-                            <Button raised accent text='Criar Conta'
-                                    onPress={() => this.props.navigation.navigate('RegisterScreen')}
-                            />
-                        </View>
-                    </View>
-                </ScrollView>
-            </KeyboardAvoidingView>
-        );
-    }
+  render () {
+    let {email, password} = this.state
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
+        <Toolbar centerElement={'Entrar com sua conta'}/>
+        <View style={styles.loginContainer}>
+          <View style={styles.fixedPadding}>
+            <TextField tintColor={mainColor} label={'Email'} value={email}
+                       onChangeText={email => this.setState({email})}/>
+            <TextField secureTextEntry={true} tintColor={mainColor} label='Senha' value={password}
+                       onChangeText={password => this.setState({password})}/>
+          </View>
+          <View style={styles.fixedPadding}>
+            <Button raised accent text='Entrar'/>
+          </View>
+          <View style={styles.linkContainer}>
+            <Text style={styles.link}
+                  onPress={() => this.props.navigation.navigate('ForgotPassword')}
+            >Esqueceu a senha?</Text>
+            <Text style={styles.link}
+                  onPress={() => this.props.navigation.navigate('RegisterScreen')}
+            >Cadastrar-se?</Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: backgroundColor,
-    },
-    logoContainer: {
-        flex: 1,
-        backgroundColor: mainColor,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    loginContainer: {
-        flex: 2,
-        padding: 40,
-    },
-    logoImage: {
-        width: 150,
-        resizeMode: 'contain',
-        marginTop: 10,
-    },
-    forgotPassword: {
-        color: accentColor,
-    },
-});
+  container: {
+    flex: 1,
+    backgroundColor: backgroundColor,
+    justifyContent: 'flex-start',
+  },
+  loginContainer: {
+    paddingTop: 15,
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  fixedPadding: {
+    paddingTop: 15,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 15,
+  },
+  link: {
+    color: accentColor,
+  },
+})
