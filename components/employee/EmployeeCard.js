4403ec39d39e91492c9c48e4535361e43834aa04
Olavo Holanda <olavohcn@gmail.com>
2019-04-09 17:49:58 -0300

    Finished home screen employees

---
+++ components/employee/EmployeeCard.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { Rating } from 'react-native-elements'
+import PropTypes from 'prop-types'
+import { Card } from 'react-native-material-ui'
+import { accentColor, lightGreyColor, mainColor } from '../../constants/Colors'
+import moment from 'moment'
+
+const styles = StyleSheet.create({
+  name: {
+    color: accentColor,
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  role: {
+    color: lightGreyColor,
+    fontStyle: 'italic',
+    fontSize: 14
+  },
+  label: {
+    color: lightGreyColor,
+    fontSize: 11
+  },
+  value: {
+    color: accentColor,
+    fontSize: 11,
+    fontWeight: 'bold',
+  }
+})
+
+class EmployeeCard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      employee: this.props.employee
+    }
+  }
+
+  renderAgeAndAdmission () {
+    const {employee} = this.state
+    const age = employee.dateOfBirth ?
+      moment().diff(moment(employee.dateOfBirth), 'years') :
+      null
+    const admAge = employee.dateOfAdmission ?
+      moment().diff(moment(employee.dateOfAdmission), 'years') :
+      null
+
+    if (age !== null && admAge !== null) {
+      return (
+        <View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.label}>Idade: </Text>
+            <Text style={styles.value}>{age} Anos</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.label}>Tempo: </Text>
+            <Text style={styles.value}>{admAge} Anos</Text>
+          </View>
+        </View>
+      )
+    }
+    return (
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{backgroundColor: accentColor, color: 'white', fontWeight: 'bold', borderRadius: 2, padding: 5}}>EXTERNO</Text>
+      </View>
+    )
+  }
+
+  render () {
+    const {employee} = this.state
+    return (
+      <View style={{flex: 1, width: '100%'}}>
+        <Card>
+          <View style={{flex: 1, flexDirection: 'row', height: 150}}>
+            <View style={{flex: 1, margin: 10, justifyContent: 'space-between'}}>
+              <View>
+                <Text style={styles.name}>{employee.name}</Text>
+                <Text style={styles.role}>{employee.category}</Text>
+              </View>
+              {this.renderAgeAndAdmission()}
+              <Rating
+                imageSize={18}
+                readonly
+                startingValue={employee.rating}
+              />
+            </View>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Image source={{uri: employee.profileUrl}} style={{width: 125, height: 125, borderRadius: 5}}/>
+            </View>
+          </View>
+        </Card>
+      </View>
+    )
+  }
+}
+
+EmployeeCard.propTypes = {
+  employee: PropTypes.object.isRequired,
+}
+
+export default EmployeeCard
--- screens/employees/HomeEmployeesScreen.js
+++ screens/employees/HomeEmployeesScreen.js
@@ -1,24 +1,114 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import { Dimensions, FlatList, ScrollView, StyleSheet, View } from 'react-native'
 import { Toolbar } from 'react-native-material-ui'
+import { accentColor, backgroundColor, mainColor } from '../../constants/Colors'
+import EmployeeCard from '../../components/employee/EmployeeCard'
+import { SceneMap, TabBar, TabView } from 'react-native-tab-view'
+
+const InternalRoute = () => (
+  <ListOfInternals/>
+)
+const ExternalRoute = () => (
+  <ListOfExternals/>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: backgroundColor,
+  },
+})
+
+const toolbarStyle = StyleSheet.create({
+  container: {
+    elevation: 0,
+  },
+})
+
+class ListOfExternals extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      workers: require('./dummyData/dummyExternalList')
+    }
+  }
+
+  render () {
+    return (
+      <ScrollView style={{flex: 1, padding: 15}}>
+        <FlatList
+          data={this.state.workers}
+          renderItem={({item}) => <EmployeeCard employee={item}/>}
+          keyExtractor={item => item.id}
+        />
+        <View style={{height: 20}}/>
+      </ScrollView>
+    )
+  }
+}
+
+class ListOfInternals extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      workers: require('./dummyData/dummyCondoList')
+    }
+  }
+
+  render () {
+    return (
+      <ScrollView style={{flex: 1, padding: 15}}>
+        <FlatList
+          data={this.state.workers}
+          renderItem={({item}) => <EmployeeCard employee={item}/>}
+          keyExtractor={item => item.id}
+        />
+        <View style={{height: 20}}/>
+      </ScrollView>
+    )
+  }
+}
 
 class HomeEmployeesScreen extends React.Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      index: 0,
+      routes: [
+        {key: 'internal', title: 'Condomínio'},
+        {key: 'external', title: 'Externo'},
+      ],
+    }
+  }
+
+  render () {
     return (
-      <View style={{flex: 1}}>
+      <View style={styles.container}>
         <Toolbar
           leftElement="menu"
           onLeftElementPress={() => this.props.navigation.toggleDrawer()}
           centerElement={'Colaboradores'}
+          style={toolbarStyle}
         />
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>
-            Home Employees Screen
-          </Text>
-        </View>
+        <TabView
+          navigationState={this.state}
+          renderScene={SceneMap({
+            internal: InternalRoute,
+            external: ExternalRoute,
+          })}
+          renderTabBar={props =>
+            <TabBar
+              {...props}
+              indicatorStyle={{backgroundColor: accentColor}}
+              style={{backgroundColor: mainColor}}
+            />
+          }
+          onIndexChange={index => this.setState({index})}
+          initialLayout={{width: Dimensions.get('window').width}}
+        />
       </View>
     )
   }
 }
 
-export default HomeEmployeesScreen;
+export default HomeEmployeesScreen
